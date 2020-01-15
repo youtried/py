10d6c8e48a8064042a7fe7af0d67694821a5880e
youtried <servic69@yandex.ru>
2020-01-15 11:43:46 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,58 @@
+p = print
 a = 10
+p (a)
+p (type(a))
 b = 5
+p (b)
+p (type(b))
 c = a + b
 d = a - b
 e = a * b
 f = a / b
 g = a % b
 h = a ** b
-p = print
-p(c)
-p(d)
-p(e)
-p(f)
-p(g)
-p(h)
-
+p (c)
+p (type(c))
+p (d)
+p (type(d))
+p (e)
+p (type(e))
+p (f)
+p (type(f))
+p (g)
+p (type(g))
+p (h)
+p (type(h))
+s1 = 'Кто ты такой,'
+s2 = ' давай дО свиданья!'
+p (s1)
+p (type(s1))
+p (s2)
+p (type(s2))
+s3 = s1 + s2
+p (s3)
+p (type(s3))
+dr1 = 10.1
+dr2 = 1.1
+dr3 = dr1 + dr2
+dr4 = dr1 - dr2
+dr5 = dr1 * dr2
+dr6 = dr1 / dr2
+dr7 = dr1 % dr2
+dr8 = dr1 ** dr2
+p (dr1)
+p (type(dr1))
+p (dr2)
+p (type(dr2))
+p (dr3)
+p (type(dr3))
+p (dr4)
+p (type(dr4))
+p (dr5)
+p (type(dr5))
+p (dr6)
+p (type(dr6))
+p (dr7)
+p (type(dr7))
+p (dr8)
+p (type(dr8))
